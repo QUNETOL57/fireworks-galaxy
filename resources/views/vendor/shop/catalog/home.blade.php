d3827aa27537cd4c5bb903f90ba8964991d0c2f1
qunetol <qunetol57@outlook.com>
2023-11-27 22:27:49 +0300

    1

---
+++ resources/views/vendor/shop/catalog/home.blade.php
@@ -0,0 +1,23 @@
+@extends('shop::base')
+
+@section('aimeos_header')
+    <?= $aiheader['locale/select'] ?? '' ?>
+    <?= $aiheader['basket/mini'] ?? '' ?>
+    <?= $aiheader['catalog/search'] ?? '' ?>
+    <?= $aiheader['catalog/tree'] ?? '' ?>
+    <?= $aiheader['catalog/home'] ?? '' ?>
+    <?= $aiheader['cms/page'] ?? '' ?>
+@stop
+
+@section('aimeos_head_nav')
+    <?= $aibody['catalog/tree'] ?? '' ?>
+@stop
+
+@section('aimeos_head_search')
+    <?= $aibody['catalog/search'] ?? '' ?>
+@stop
+
+@section('aimeos_body')
+    <?= $aibody['catalog/home'] ?? '' ?>
+    <?= $aibody['cms/page'] ?? '' ?>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile/me', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile/me', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile/me', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', '\Aimeos\Shop\Controller\CatalogController@homeAction')->name('aimeos_home');
+});
+
+require __DIR__.'/auth.php';
